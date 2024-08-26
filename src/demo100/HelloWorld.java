476736d2cdd70fc7b94b191876696d847e1381f1
afodor <anthony.fodor@gmail.com>
2024-08-26 13:38:04 -0400

    Update HelloWorld.java

--- src/demo100/HelloWorld.java
+++ src/demo100/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello.  It is Tuesday 1:27 pm");
+		System.out.println("Hello. Actually it is Monday");
 	}
 
 	public static void foo()
